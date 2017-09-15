9f5df957805559967a5a6ae5fef1589008b3ef7a
AdrianMindak <adrian.richard.mindak@gmail.com>
2017-09-15 09:52:47 +0200

    first steps
    bring order in src
    add components
    renamed styles and index
    rerefereced
    add header, content and menue to app

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className='app-header'></div>
+        <div className='app-content'></div>
+        <div className='app-menue'></div>
+      </div>
+    );
+  }
+}
